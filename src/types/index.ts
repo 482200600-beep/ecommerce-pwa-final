706d1f296ce591db7ceb51cef5fcad33d12cbd76
482200600-beep <482200600@alumnos.utzac.edu.mx>
2025-11-21 00:13:49 -0600

    Update index.ts

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,1 +1,20 @@
+export interface Product {
+  id: number;
+  name: string;
+  price: number;
+  description: string;
+  image: string;
+  category: string;
+}
 
+export interface User {
+  id: string;
+  name: string;
+  email: string;
+  avatar?: string;
+}
+
+export interface CartItem {
+  product: Product;
+  quantity: number;
+}
