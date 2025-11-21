e74475898b58f0dfdb0f0bfb0990e21910ba6a2a
482200600-beep <482200600@alumnos.utzac.edu.mx>
2025-11-21 00:15:01 -0600

    Update ProductCard.tsx

--- src/components/ProductCard.tsx
+++ src/components/ProductCard.tsx
@@ -1,1 +1,28 @@
+import React from 'react';
+import { Product } from '../types';
 
+interface ProductCardProps {
+  product: Product;
+  onAddToCart: (product: Product) => void;
+}
+
+const ProductCard: React.FC<ProductCardProps> = ({ product, onAddToCart }) => {
+  return (
+    <div className="product-card">
+      <img src={product.image} alt={product.name} />
+      <h3>{product.name}</h3>
+      <p>{product.description}</p>
+      <div className="product-footer">
+        <span className="price">${product.price}</span>
+        <button 
+          className="add-to-cart"
+          onClick={() => onAddToCart(product)}
+        >
+          Agregar al Carrito
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default ProductCard;
